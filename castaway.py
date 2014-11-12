5c5427f74cbe82b1de743bd30542c840dae87da7
Erik Lax <erik@datahack.se>
2014-11-12 21:54:38 +0100

    more things, still not stable

--- castaway.py
+++ castaway.py
@@ -65,7 +65,11 @@ class ChromeCast(SimpleHTTPServer.SimpleHTTPRequestHandler):
 			self.send_response(200)
 			self.send_header('Content-Type', 'application/json')
 			self.end_headers()
-			self.wfile.write(json.dumps([{'name': p.name, 'uuid': p.uuid} for p in playList.items]))
+			l = dict()
+			l['tracks'] = [{'name': p.name, 'uuid': p.uuid} for p in playList.items]
+			l['repeat'] = playList.repeat
+			l['shuffle'] = playList.shuffle
+			self.wfile.write(json.dumps(l))
 			return
 
 		# skip-to a uuid
